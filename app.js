0f41c81eaca3a4dca39f737616f2fad9ca7a5083
RuthCodina <ruth0066@hotmail.com>
2024-05-29 08:38:34 -0500

    post

---
+++ app.js
@@ -0,0 +1,41 @@
+require('dotenv').config()
+
+const express = require('express')
+const expressLayout = require('express-ejs-layouts')
+const cookieParser = require('cookie-parser');
+const session = require('express-session')
+const connectDB = require('./server/config/db')
+const mongoStore = require('connect-mongo')
+const app = express()
+const PORT = process.env.PORT || 5000
+
+//connecting to DB
+connectDB()
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+app.use(session({
+    secret: 'jelou',
+    resave: false,
+    saveUninitialized: true,
+    store: mongoStore.create({
+      mongoUrl: process.env.MONGODB_URI
+    }),
+    //cookie: { maxAge: new Date ( Date.now() + (3600000) ) } 
+  }));
+//app.use(methodOverride('_method'));
+app.use(express.static('public'))
+
+//templing engine
+app.use(expressLayout)
+app.set('layout', './layouts/main')
+app.set('view engine', 'ejs')
+
+//routing
+app.use('/', require('./server/routes/main'))
+app.use('/', require('./server/routes/admin'))
+
+app.listen(PORT, ()=> { 
+    console.log(`App listening on port ${PORT}`)
+})
---
+++ server/routes/main.js
@@ -0,0 +1,70 @@
+const express = require('express')
+const router = express.Router()
+const post = require('../models/posts')
+
+router.get('', async (req, res) => {
+    const locals = {
+        title: 'Node Blog',
+        description:'Blog created with nodejs, express y mongodb'
+    }
+    try {
+        const data = await post.find()
+        res.render('index', {locals, data})
+    } catch(error) {
+        console.log(error)
+    }
+})
+
+router.get('/post/:id', async (req, res) => {
+    try {
+        const locals = {
+            title: 'Node Blog',
+            description:'Blog created with nodejs, express y mongodb'
+        }
+        let slug = req.params.id
+        const data = await post.findById({_id:slug})
+        res.render('post', {locals, data, currentRoute:`/post/${slug}`})
+    } catch(error) {
+        console.log(error)
+    }
+})
+
+router.post('/search', async (req, res) => {
+    try {
+        const locals = {
+          title: "Search",
+          description: "Simple Blog created with NodeJs, Express & MongoDb."
+        }
+    
+        let searchTerm = req.body.searchTerm;
+        const searchNoSpecialChar = searchTerm.replace(/[^a-zA-Z0-9 ]/g, "")
+    
+        const data = await post.find({
+          $or: [
+            { title: { $regex: new RegExp(searchNoSpecialChar, 'i') }},
+            { body: { $regex: new RegExp(searchNoSpecialChar, 'i') }}
+          ]
+        });
+    
+        res.render("search", {data, locals, currentRoute: '/'});
+    
+      } catch (error) {
+        console.log(error);
+      }
+})
+
+router.get('/about', (req, res) => {
+    res.render('about')
+})
+
+/*function insertPostData(){
+    post.insertMany([
+        {
+            title:"Building a Blog",
+            body: "This is the body text"
+        },
+    ])
+}
+insertPostData()*/
+
+module.exports = router
